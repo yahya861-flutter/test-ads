fa4f7bb6404e1431a59dec9a19536582fd914626
yahya861-flutter <yousafrana1212@gmail.com>
2026-02-04 13:07:48 +0500

    ads

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: NativeDesignsScreen());
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: home());
   }
 }
 
